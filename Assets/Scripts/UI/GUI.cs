9dde650ed6a00068c94dcfb471ae870b3fd33b57
Dawid Kreft <s184484@student.pg.edu.pl>
2023-02-06 16:34:19 +0100

    Added camera view indicator

--- Assets/Scripts/UI/GUI.cs
+++ Assets/Scripts/UI/GUI.cs
@@ -14,6 +14,23 @@ public class GUI : MonoBehaviour
     public float height;
     public float ratio;
 
+    //minimap viewport
+    public GameObject cam;
+    public float viewportWorldWidth;
+    public float viewportWorldHeight;
+
+    public float viewportWorldPosX;
+    public float viewportWorldPosY;
+
+    public GameObject prefab;
+
+    public float viewportNormalizedPosX;
+    public float viewportNormalizedPosY;
+
+    public float viewportNormalizedWidth;
+    public float viewportNormalizedHeight;
+
+    public GameObject minimapIndicator;
 
 
     // Start is called before the first frame update
@@ -27,6 +44,41 @@ public class GUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (cam != null)
+        {
+            viewportWorldWidth = cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(1, 1, Mathf.Abs(cam.transform.position.z))).x - cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0, 0, Mathf.Abs(cam.transform.position.z))).x;
+            viewportWorldHeight = cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(1, 1, Mathf.Abs(cam.transform.position.z))).y - cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0, 0, Mathf.Abs(cam.transform.position.z))).y;
+
+
+            viewportWorldPosX = cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Mathf.Abs(cam.transform.position.z))).x;
+            viewportWorldPosY = cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Mathf.Abs(cam.transform.position.z))).y;
+        }
+        
+
+        viewportNormalizedPosX = viewportWorldPosX / 80;
+        viewportNormalizedPosY = viewportWorldPosY / 50;
+
+        viewportNormalizedWidth = viewportWorldWidth / 80;
+        viewportNormalizedHeight = viewportWorldHeight / 50;
+
+        minimapIndicator.GetComponent<RectTransform>().anchoredPosition = new Vector2((viewportNormalizedPosX * 200)-100, (viewportNormalizedPosY * 125)-100+ (200-125)/2);
+        minimapIndicator.GetComponent<RectTransform>().sizeDelta= new Vector2(viewportNormalizedWidth*200, viewportNormalizedHeight*125);
+
+ 
+        /*
+        if (Input.GetKey(KeyCode.P))
+        {
+            Color customColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1.0f);
+            GameObject cube1= Instantiate(prefab, cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0, 0, Mathf.Abs(cam.transform.position.z))), Quaternion.identity);
+            cube1.GetComponent<Renderer>().material.SetColor("_Color", customColor);
+            GameObject cube2 = Instantiate(prefab, cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0, 1, Mathf.Abs(cam.transform.position.z))), Quaternion.identity);
+            cube2.GetComponent<Renderer>().material.SetColor("_Color", customColor);
+            GameObject cube3 = Instantiate(prefab, cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(1, 0, Mathf.Abs(cam.transform.position.z))), Quaternion.identity);
+            cube3.GetComponent<Renderer>().material.SetColor("_Color", customColor);
+            GameObject cube4 = Instantiate(prefab, cam.GetComponent<Camera>().ViewportToWorldPoint(new Vector3(1, 1, Mathf.Abs(cam.transform.position.z))), Quaternion.identity);
+            cube4.GetComponent<Renderer>().material.SetColor("_Color", customColor);
+        }*/
+
         if (tileMap.miniMapTexture != null && isLoaded==false)
         {
             tileMapTexture = tileMap.miniMapTexture;
@@ -34,8 +86,19 @@ public class GUI : MonoBehaviour
             width = tileMap.width;
             isLoaded = true;
             miniMapImage.GetComponent<RawImage>().texture = tileMapTexture;
-            ratio = height / width;
-            miniMapImage.GetComponent<RectTransform>().sizeDelta = new Vector2(200, ratio * 200);
+            
+            if (width >= height)
+            {
+                ratio = height / width;
+                miniMapImage.GetComponent<RectTransform>().sizeDelta = new Vector2(200, ratio * 200);
+            }
+            else
+            {
+                ratio = width / height;
+                miniMapImage.GetComponent<RectTransform>().sizeDelta = new Vector2(ratio * 200, 200);
+            }
+            
+            
         }
         
     }
