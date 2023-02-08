d94528e7357004af2a9c39590160d62cbc415323
Dawid Kreft <s184484@student.pg.edu.pl>
2023-02-08 13:47:07 +0100

    Improved cursor info

--- Assets/Scripts/UI/CursorInfo.cs
+++ Assets/Scripts/UI/CursorInfo.cs
@@ -20,6 +20,8 @@ public class CursorInfo : MonoBehaviour
     public bool isMoved;
     public bool isSaved;
 
+    public float correctionX, correctionY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,8 +41,11 @@ public class CursorInfo : MonoBehaviour
         float dispAreaEndX = dispAreaPosX + (dispAreaWidth / 2);
         float dispAreaEndY = dispAreaPosY + (dispAreaHeight / 2);
 
+        float infoPanelWidth = (infoPanel.GetComponent<RectTransform>().anchorMax.x - infoPanel.GetComponent<RectTransform>().anchorMin.x) * Screen.width + infoPanel.GetComponent<RectTransform>().sizeDelta.x * gui.GetComponent<Canvas>().scaleFactor;
+        float infoPanelHeight = (infoPanel.GetComponent<RectTransform>().anchorMax.y - infoPanel.GetComponent<RectTransform>().anchorMin.y) * Screen.height + infoPanel.GetComponent<RectTransform>().sizeDelta.y * gui.GetComponent<Canvas>().scaleFactor;
 
-        if(Input.mousePosition.x>= dispAreaOriginX&& Input.mousePosition.x<= dispAreaEndX)
+
+        if (Input.mousePosition.x>= dispAreaOriginX&& Input.mousePosition.x<= dispAreaEndX)
         {
             if (Input.mousePosition.y >= dispAreaOriginY && Input.mousePosition.y <= dispAreaEndY)
             {
@@ -57,9 +62,35 @@ public class CursorInfo : MonoBehaviour
         }
 
         //Debug.Log(dispAreaOriginX+" "+Input.mousePosition.x);
-
+        
         if (Input.GetMouseButton(1) && isOverDispArea)
         {
+            correctionY = 0;
+            correctionX = 0;
+
+            if (Screen.height-Input.mousePosition.y < infoPanelHeight/2)
+            {
+                correctionY = -Input.mousePosition.y+Screen.height- infoPanelHeight / 2;
+            }
+            
+            if (Input.mousePosition.y < infoPanelHeight / 2)
+            {
+                correctionY = -Input.mousePosition.y + infoPanelHeight / 2;
+            }
+            
+            if (Screen.width - Input.mousePosition.x < infoPanelWidth / 2)
+            {
+                correctionX = -Input.mousePosition.x + Screen.width - infoPanelWidth / 2;
+            }
+            
+            if (Input.mousePosition.x < infoPanelWidth / 2)
+            {
+                correctionX = -Input.mousePosition.x + infoPanelWidth / 2;
+            }
+            
+            //Debug.Log(Screen.height - Input.mousePosition.y);
+            //Debug.Log(infoPanelHeight);
+            
             if (!isSaved)
             {
                 mousePosTmp = Input.mousePosition;
@@ -73,10 +104,24 @@ public class CursorInfo : MonoBehaviour
 
             if (!isMoved)
             {
-                objectName.text = mouseSelection.highlightedTile.data.name;
-                objectDescription.text = mouseSelection.highlightedTile.data.description;
+                if (mouseSelection.highlightedTile.contents.armies != null)
+                {
+                    objectName.text = "Army";
+                    objectDescription.text = "Army";
+                }
+                else if (mouseSelection.highlightedTile.contents.city != null)
+                {
+                    objectName.text = "City";
+                    objectDescription.text = "City";
+                }
+                else
+                {
+                    objectName.text = mouseSelection.highlightedTile.data.name;
+                    objectDescription.text = mouseSelection.highlightedTile.data.description;
+                }
+                
                 //Debug.Log(gameObject.GetComponent<RectTransform>().anchoredPosition.x * gui.GetComponent<Canvas>().scaleFactor);
-                infoPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(Input.mousePosition.x - Screen.width / 2, Input.mousePosition.y - Screen.height / 2) / gui.GetComponent<Canvas>().scaleFactor;
+                infoPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2((Input.mousePosition.x+correctionX) - Screen.width / 2, (Input.mousePosition.y+correctionY) - Screen.height / 2) / gui.GetComponent<Canvas>().scaleFactor;
                 Cursor.visible = false;
                 infoPanel.SetActive(true);
             }
